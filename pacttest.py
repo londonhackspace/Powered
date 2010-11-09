da50874e7b81317fe27be5f07b679111682b3ce2
Mark Steward <marksteward@gmail.com>
2010-11-09 03:06:11 +0000

    PACT test script and Powered initial commit

---
+++ pacttest.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+import serial
+s = None
+
+def open():
+  global s
+  s = serial.Serial('/dev/ttyS0', 1200, timeout=1)
+
+def close():
+  s.close()
+
+def testLine(str, max=100):
+
+  s.write(str)
+  s.write('\r')
+
+  res = []
+  while True:
+    r = s.read(max)
+    if not r: break
+    res.append(' '.join(['%02x' % ord(x) for x in r]))
+
+  return '\n'.join(res)
+
+def getLine(str):
+
+  s.write(str)
+  res = []
+  while True:
+    c = s.read(1)
+    if not c: break
+    res.append(c)
+    if c == '\r': break
+
+  return ''.join(res)
+ 
+
+if False:
+  open()
+  print testLine('\0' * 20) # '\0' * 9 + '\r'
+  print testLine('\0' * 20) # '\0' * 9 + '\r'
+  print testLine('\0' * 10) # '\0' * 10 + '\r'
+  print testLine('\0' * 1)  # '\0' + '\r'
+  close()
+
+if False:
+  open()
+  for i in range(20, 30):
+    print i
+    print testLine('\0' * i)
+  close()
+
+  # '\0' * i + '\r' if i < 11
+  # '\0' * (i - 11) + '\r' if i >= 11
+  # '\0' * (i - 22) + '\r' if i >= 22
+
+  # Seems to use packets of 11 chars
+
+
+if False:
+  open()
+  print testLine('\xff' * 20) # '\x8c' * 5
+  print testLine('\x01' * 20) # '\x01' * 9
+  print testLine('\x80' * 20) # '\xfc'
+
+  print testLine('\x96' * 20) # '\xff'
+  print testLine('\xa5' * 20) # '\xa5' * 10
+  close()
+
+  # Seems to be PDU based
+
+if False:
+  def ascii(s):
+    return ''.join([chr(int(x, 16)) for x in s.split(' ')])
+
+  for x in range(255):
+    open()
+    r = testLine(chr(x))
+    if r != '%02x 0d' % x:
+      print '%02x %s' % (x, repr(chr(x)))
+      if not r:
+        print 'blank'
+      else:
+        print '%s %s' % (r, repr(ascii(r)))
+    close()
+
+  # 0d '\r'
+  # 0d '\r'
+  # 0d '\r'
+  #
+  # 46 'F'
+  # 46 3d 34 39 2e 39 37 39 0d 'F=49.979\r'
+  # 46 3d 34 39 2e 39 38 36 0d 'F=49.986\r'
+  # 46 3d 34 39 2e 39 36 37 0d 'F=49.967\r'
+  #
+  # 4e 'N'
+  #
+  # ff '\xff'
+  # ff '\xff'
+  #
+  # 52 'R'
+  # 52 3d 31 34 3a 35 30 3a 33 32 0d 'R=14:50:32\r'
+  # 52 3d 31 35 3a 30 30 3a 34 30 0d 'R=15:00:40\r'
+  # 52 3d 31 35 3a 32 31 3a 30 31 0d 'R=15:21:01\r'
+  #
+  # 53 'S'
+  # 53 50 39 39 41 30 31 38 36 39 0d 'SP99A01869\r'
+  # 53 50 39 39 41 30 31 38 36 39 0d 'SP99A01869\r'
+  # 53 50 39 39 41 30 31 38 36 39 0d 'SP99A01869\r'
+  #
+  # 73 's'
+  # 73 3d 9e 01 05 a6 00 00 00 00 0d 's=\x9e\x01\x05\xa6\x00\x00\x00\x00\r'
+  # 73 3d 9e 01 05 a6 00 00 00 00 0d 's=\x9e\x01\x05\xa6\x00\x00\x00\x00\r'
+  # 73 3d 9e 01 05 a6 00 00 00 00 0d 's=\x9e\x01\x05\xa6\x00\x00\x00\x00\r'
+  #
+  # 75 'u'
+  # 55 0d 'U\r'
+  # 55 0d 'U\r'
+  # 55 0d 'U\r'
+  #
+  # 78 'x'
+  # ff '\xff'
+  # ff '\xff'
+  # ff '\xff'
+  #
+  # 80 '\x80'
+  # fc '\xfc'
+  # fc '\xfc'
+  # fc '\xfc'
+  #
+  # 81 '\x81'
+  # ff '\xff'
+  # ff '\xff'
+  # ff '\xff'
+  #
+  # 84 '\x84'
+  # 83 0d 84 '\x83\r\x84'
+  # 83 0d 84 '\x83\r\x84'
+  # 83 0d 84 '\x83\r\x84'
+  #
+  # 87 '\x87'
+  # ff '\xff'
+  # ff '\xff'
+  # ff '\xff'
+  #
+  # All others between 130 and 254 return nothing
+
+  # So looks like an ASCII protocol, lines up to 10 chars
+
+
+if False:
+
+  open()
+  for x in range(0x20, 0x7f):
+    print '%02x' % x
+    for y in range(0x20, 0x7f):
+      for z in range(0x20, 0x7f):
+        t = ''.join(map(chr, [x, y, z, 0x0d]))
+        r = getLine(t)
+        if r != t:
+          print t
+          print r
+
+  close()
+
+  # See documentation on wiki
+
+if False:
+
+  import time
+
+  open()
+  start = time.time()
+
+  for i in range(0, 100):
+    r = getLine('UA\r') # 12.4657409191
+    #r = getLine('UA\r') # 12.4215011597
+    #r = getLine('F\r') # 14.2507390976
+    #r = getLine('F\r') # 14.1550590992
+    #r = getLine('X00\r') # 16.90279603
+    #r = getLine('X00\r') # 16.7112660408
+    #r = getLine('AAAAAAAAAA\r') # 24.0108280182
+
+  end = time.time()
+  close()
+
+  print end - start
+
+if True:
+
+  import time
+  open()
+
+  try:
+    while True:
+      o = []
+      for i in range(0x30, 0x40):
+        r = getLine('X%02X\r' % i)
+        o.append(r)
+
+      print '\n' * 30
+      print '\n'.join(o)
+      time.sleep(1)
+
+  except:
+    close()
+    raise
---
+++ powered.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+import re
+import sys
+import time
+import serial
+import struct
+import datetime
+
+import urlparse
+import BaseHTTPServer
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read((
+    'powered.conf',
+    sys.path[0] + '/powered.conf',
+    '/etc/powered.conf'
+))
+
+PORT = config.getint('powered', 'tcpport')
+DEV = config.get('powered', 'serialport')
+
+class PactMeter:
+
+    def __init__(self, dev='/dev/ttyS0'):
+        self.dev = dev
+        self.open()
+
+    def open(self):
+        self.s = serial.Serial(self.dev, 1200, timeout=1)
+
+    def close(self):
+        self.s.close()
+
+    def writeline(self, str):
+        self.s.write(str + '\r')
+
+    def readline(self):
+
+        res = []
+        while True:
+            c = self.s.read(1)
+            if not c: break # TODO: raise exception?
+            if c == '\r': break
+            res.append(c)
+
+        return ''.join(res)
+
+    def parse(self, code, ret):
+        fromhex  = lambda x: int(x, 16)
+        fromtime = lambda x: datetime.time(*time.strptime(x, '%H:%M:%S')[3:6])
+        frompack = lambda x: struct.unpack('<II', x)
+        oddfloat = lambda x, s: float(s + x)
+
+        parsers = [
+          ('F=(\d+\.\d+)',                float),
+          ('R=(\d\d:\d\d:\d\d)',          fromtime),
+          ('S([0-9A-Z]{9})',              str),
+          ('s=(.{8})',                    frompack),
+          ('B[AVWvw]([0-9A-F]{6})',       fromhex),
+          ('[IiLPpV][123]([+-]\d+\.\d+)', float),
+          ('K[123AVW]([+-]\d+\.\d+)',     float),
+          ('Q[123A]=(\d+\.\d+)([+-])',    oddfloat),
+          ('U[AVWvw]=(\d{7})',            int),
+          ('X[0-9A-F]{2}=([0-9A-F]{6})',  fromhex),
+        ]
+
+        for pattern, parser in parsers:
+            m = re.match(pattern, ret)
+            if m:
+                return parser(*m.groups())
+
+        
+
+class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    # Disable logging DNS lookups
+    def address_string(self):
+        return str(self.client_address[0])
+
+    def do_GET(self):
+        self.url = urlparse.urlparse(self.path)
+        self.params = urlparse.parse_qs(self.url.query)
+
+        def html_ok():
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+        def do_index(path, args):
+            html_ok()
+            self.wfile.write('Usage: /UA (temporarily)')
+
+        def do_meter(code):
+            html_ok()
+            self.wfile.flush()
+
+            meter.writeline(code)
+            ret = meter.readline()
+            print 'Meter returned: %s' % ret
+            val = meter.parse(code, ret)
+            self.wfile.write(val)
+            self.wfile.write('\n')
+
+        dispatches = [
+            ('^/$',                do_index),
+            ('^/([FRSs])$',        do_meter),
+            ('^/(B[AVWvw])$',      do_meter),
+            ('^/([IiLPpV][123])$', do_meter),
+            ('^/(K[123AVW])$',     do_meter),
+            ('^/(Q[123A])$',       do_meter),
+            ('^/([Uu][AVWvw])$',   do_meter),
+            ('^/(X[0-9A-F]{2})$',  do_meter),
+        ]
+        for pattern, dispatch in dispatches:
+            m = re.match(pattern, self.path)
+            if m:
+                dispatch(*m.groups())
+                break
+
+        else:
+            self.send_error(500)
+            self.end_headers()
+            self.wfile.write('Bad request, should be in the format /UA\n')
+
+
+
+
+meter = PactMeter(DEV)
+httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
+httpd.serve_forever()
+
